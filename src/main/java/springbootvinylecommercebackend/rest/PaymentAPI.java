3d1524df2933881b712dc8c8cbc5aad4e469c4cb
Ky Nguyen <cuty0406@outlook.com>
2025-02-10 17:44:57 +0700

    17: Create VNPay service and config

---
+++ src/main/java/springbootvinylecommercebackend/rest/PaymentAPI.java
@@ -0,0 +1,28 @@
+package springbootvinylecommercebackend.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/payment")
+public class PaymentAPI {
+
+//    @GetMapping("/vn-pay")
+//    public ResponseEntity<?> pay() {
+//        return new ResponseObject<>(HttpStatus.OK, "Success", paymentService.createVnPayPayment(request));
+//    }
+//    @GetMapping("/vn-pay-callback")
+//    public ResponseObject<PaymentDTO.VNPayResponse> payCallbackHandler(HttpServletRequest request) {
+//        String status = request.getParameter("vnp_ResponseCode");
+//        if (status.equals("00")) {
+//            return new ResponseObject<>(HttpStatus.OK, "Success", new PaymentDTO.VNPayResponse("00", "Success", ""));
+//        } else {
+//            return new ResponseObject<>(HttpStatus.BAD_REQUEST, "Failed", null);
+//        }
+//    }
+
+}
---
+++ src/main/java/springbootvinylecommercebackend/service/VNPayService.java
@@ -0,0 +1,9 @@
+package springbootvinylecommercebackend.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public interface VNPayService {
+    String createVnPayPayment(HttpServletRequest request);
+
+
+}
